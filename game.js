66d0e5dc28984c7a5030b66534b810ec8d0aeeb7
Eran Usha <eusha5@gmail.com>
2018-08-28 15:37:53 -0400

    updated styling

--- game.js
+++ game.js
@@ -1,11 +1,44 @@
+var theGame;
+
+window.onload = function() {
 
 //Global Variables
-
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var theInt;
 
- class Player {
+class Game{
+    constructor () {
+        this.player = new Player();
+        this.obstacles = [];
+    }
+
+    obstacleCollisionCheck() {
+        this.obstacles.forEach((eachObstacle) => {
+            
+            if ((this.player.x + this.player.width >= eachObstacle.x && this.player.x <= eachObstacle.x + eachObstacle.width) &&
+                (this.player.y + this.player.height >= eachObstacle.y && this.player.y <= eachObstacle.y + eachObstacle.height)){
+                //here is where we can remove points, or gain points, or bound off the boundaries
+                }
+        }) 
+    }
+    
+    drawEverything() {
+        this.player.drawPlayer();
+        this.obstacles.forEach((oneObst) => {
+            oneObst.drawObstacle();
+        })
+    }
+    
+    generateNewObstacle() {
+        const theX = 750;
+        const theY = Math.floor(Math.random()*275);
+        this.obstacles.unshift(new Obstacle(theX, theY))
+        this.obstacles[0].moveObstacle();
+    }
+}
+
+class Player {
     constructor(){
         this.x = 40;
         this.y = 200;
@@ -18,107 +51,113 @@ var theInt;
     drawPlayer() {
         var img = new Image();
         img.src = this.img;
-        img.onload = (() => {
+        // img.onload = (() => {
         ctx.drawImage(img, this.x, this.y,this.width, this.height);
-        })
+        // })
     }
-
-    movePlayer(number) {
-        ctx.clearRect(this.x, this.y, this.width, this.height );
-        switch(number){
-            case 37:
-            if (this.x > 0) {
-                this.x -= 5;
-            }break;
-            case 39:
-            if (this.x < canvas.width-75){
-                this.x += 5;
-            }else{
-                this.x += 0;
+        
+        movePlayer(number) {
+            ctx.clearRect(this.x, this.y, this.width, this.height );
+            switch(number){
+                case 37:
+                if (this.x > 0) {
+                    this.x -= 5;
+                }break;
+                case 39:
+                if (this.x < canvas.width-75){
+                    this.x += 5;
+                }else{
+                    this.x += 0;
+                }
             }
+            this.drawPlayer();
         }
-        this.drawPlayer();
-    }
-           
-    jumpPlayer(keyCode) {
-
-        clearInterval(theInt);
-        ctx.clearRect(this.x, this.y, this.width, this.height );
-        if (keyCode === 32) {
-            if (this.y > 50){
-            this.y -= 50;
-            this.drawPlayer();                
-            theInt = setInterval(() => {
-                if(this.y < 200){
-                    ctx.clearRect(this.x, this.y, this.width, this.height);
-                    this.y += 5    
-                    this.drawPlayer(); 
+        
+        jumpPlayer(keyCode) {
+            clearInterval(theInt);
+            ctx.clearRect(this.x, this.y, this.width, this.height );
+            if (keyCode === 32) {
+                if (this.y > 50){
+                    this.y -= 50;
+                    this.drawPlayer();                
+                    theInt = setInterval(() => {
+                        if(this.y < 200){
+                            ctx.clearRect(this.x, this.y, this.width, this.height);
+                            this.y += 5    
+                            this.drawPlayer(); 
+                        }
+                    }, 50);
+                }else{
+                    this.y=50;
+                        this.drawPlayer();   
+                    theInt = setInterval(() => {
+                        if(this.y < 200){
+                            ctx.clearRect(this.x, this.y, this.width, this.height);
+                            this.y += 5    
+                            this.drawPlayer(); 
+                        }
+                    }, 50);             
                 }
-            }, 50);
-            }else{
-                this.y=50;
-              this.drawPlayer();   
-              theInt = setInterval(() => {
-                if(this.y < 200){
-                    ctx.clearRect(this.x, this.y, this.width, this.height);
-                    this.y += 5    
-                    this.drawPlayer(); 
-                }
-            }, 50);             
-            }
+            } 
         } 
-     } 
-    
 }        
 
- class Obstacles {
-     constructor() {
-        // this.width = 50;
-        // this.height = 45;
-        this.img1 = 'images/mushroom-obstacle.png';
-        this.img2 = 'images/sapling-obstacle.png';
+class Obstacle {
+     constructor(theX, theY, theWidth, theHeight) {
+        this.x = theX;
+        this.y = theY;
+        this.width = theWidth;
+        this.height = theHeight;
+        this.mush = 'images/mushroom-obstacle.png';
+        this.sap = 'images/sapling-obstacle.png';
+        this.image = ['images/mushroom-obstacle.png', 'images/sapling-obstacle.png']
         // this.type1 = 
         // this.type2 = 
-     }
- 
-        drawMushroomObstacle() {
-            var img1 = new Image();
-            img1.src = this.img1;
-            img1.onload= (() => {
-                ctx.drawImage(img1, 500, 262, 35, 30);
+    }
+    
+    drawObstacle() {
+        var mush = new Image();
+        var sap = new Image();
+        mush.src = this.image[Math.floor(Math.random() * this.image.length)];
+        mush.onload= (() => {
+                ctx.drawImage(mush, 750, Math.floor(Math.random()*275), 45, 40);
+                ctx.drawImage(sap, 750, Math.floor(Math.random()*275), 65, 50);
             })
-        }
-
-        drawSaplingObstacle() {
-            var img2 = new Image();
-            img2.src = this.img2;
-            img2.onload= (() => {
-                ctx.drawImage(img2, 600, 250, 50, 45);
-            })
-        }
-
-//      drawObstacles() {
-//         ctx.clearRect(this.x, this.y, this.width, this.height );
+    }
         
-//         if (game is active - start button clicked) {
-//             drawMushroomObstacle && drawSaplingObstacle 
-//             move both obstacles along x axis from max-x to min-x on an infinite loop, at randomly selected y-axis points
+    moveObstacle() {
+        setInterval(() => {
+            this.x-=25;
+        },50)
+    }
+}
+    
+let frames = 0;
+    
+function animate() {
+    setInterval(() => {
+        theGame.drawEverything();
+        if(frames % 2500 === 0) theGame.generateNewObstacle();
+        ctx.clearRect(0,0,800,400);
+    theGame.obstacleCollisionCheck();
+    frames++;
+}, 1000)
+}
 
-//         }
+//once this is working, use requestanimationframe instead of setinterval to stop the flickering
 
-//         this.drawMushroomObstacle();
-//         this.drawSaplingObstacle();
-//     }
-// }
- 
- }
+function startGame() {
+    theGame = new Game();
+}
 
-window.onload = function() {
+document.getElementById("btn-start").onclick = function() {
+    startGame();
+    animate();
+    theGame = new Game();
     player = new Player();
     player.drawPlayer();
-    obstacles = new Obstacles();
-    obstacles.drawMushroomObstacle();
-    obstacles.drawSaplingObstacle();
+    obstacles = new Obstacle();
+    obstacles.drawObstacle();
 }
 
 document.onkeydown = function(e) {
@@ -129,5 +168,7 @@ document.onkeydown = function(e) {
     }
     if (move === 32) {
         player.jumpPlayer(move);
-        }  
+    }
+}
+
 }
