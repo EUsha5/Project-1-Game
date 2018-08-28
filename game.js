d485fed58b7651ec80a222f69f05bc1fb9d81123
Eran Usha <eusha5@gmail.com>
2018-08-28 17:27:53 -0400

    updated JS functionality

--- game.js
+++ game.js
@@ -15,24 +15,26 @@ class Game{
 
     obstacleCollisionCheck() {
         this.obstacles.forEach((eachObstacle) => {
-            
             if ((this.player.x + this.player.width >= eachObstacle.x && this.player.x <= eachObstacle.x + eachObstacle.width) &&
                 (this.player.y + this.player.height >= eachObstacle.y && this.player.y <= eachObstacle.y + eachObstacle.height)){
-                //here is where we can remove points, or gain points, or bound off the boundaries
-                }
+            }
+            if(eachObstacle.x === -75) {
+                this.obstacles.pop(eachObstacle);
+            }
         }) 
     }
     
     drawEverything() {
         this.player.drawPlayer();
-        this.obstacles.forEach((oneObst) => {
-            oneObst.drawObstacle();
+        this.obstacles.forEach((oneObsticle) => {
+            oneObsticle.drawObstacle();
         })
     }
     
     generateNewObstacle() {
         const theX = 750;
         const theY = Math.floor(Math.random()*275);
+
         this.obstacles.unshift(new Obstacle(theX, theY))
         this.obstacles[0].moveObstacle();
     }
@@ -44,8 +46,9 @@ class Player {
         this.y = 200;
         this.width = 80;
         this.height = 95;
-        this.hp = 100;
         this.img = 'images/teemo-player.png';
+        this.score = 0;
+        this.health = 100;
     }
     
     drawPlayer() {
@@ -57,7 +60,7 @@ class Player {
     }
         
         movePlayer(number) {
-            ctx.clearRect(this.x, this.y, this.width, this.height );
+            // ctx.clearRect(this.x, this.y, this.width, this.height );
             switch(number){
                 case 37:
                 if (this.x > 0) {
@@ -70,31 +73,31 @@ class Player {
                     this.x += 0;
                 }
             }
-            this.drawPlayer();
+            // this.drawPlayer();
         }
         
         jumpPlayer(keyCode) {
             clearInterval(theInt);
-            ctx.clearRect(this.x, this.y, this.width, this.height );
+            // ctx.clearRect(this.x, this.y, this.width, this.height );
             if (keyCode === 32) {
                 if (this.y > 50){
                     this.y -= 50;
-                    this.drawPlayer();                
+                    // this.drawPlayer();                
                     theInt = setInterval(() => {
                         if(this.y < 200){
-                            ctx.clearRect(this.x, this.y, this.width, this.height);
+                            // ctx.clearRect(this.x, this.y, this.width, this.height);
                             this.y += 5    
-                            this.drawPlayer(); 
+                            // this.drawPlayer(); 
                         }
                     }, 50);
                 }else{
                     this.y=50;
-                        this.drawPlayer();   
+                        // this.drawPlayer();   
                     theInt = setInterval(() => {
                         if(this.y < 200){
-                            ctx.clearRect(this.x, this.y, this.width, this.height);
+                            // ctx.clearRect(this.x, this.y, this.width, this.height);
                             this.y += 5    
-                            this.drawPlayer(); 
+                            // this.drawPlayer(); 
                         }
                     }, 50);             
                 }
@@ -103,32 +106,29 @@ class Player {
 }        
 
 class Obstacle {
-     constructor(theX, theY, theWidth, theHeight) {
+     constructor(theX, theY) {
         this.x = theX;
         this.y = theY;
-        this.width = theWidth;
-        this.height = theHeight;
-        this.mush = 'images/mushroom-obstacle.png';
-        this.sap = 'images/sapling-obstacle.png';
+        this.width = 50;
+        this.height = 50;
         this.image = ['images/mushroom-obstacle.png', 'images/sapling-obstacle.png']
-        // this.type1 = 
-        // this.type2 = 
+        this.imgsrc = this.image[Math.floor(Math.random() * 2)]
     }
     
     drawObstacle() {
-        var mush = new Image();
-        var sap = new Image();
-        mush.src = this.image[Math.floor(Math.random() * this.image.length)];
-        mush.onload= (() => {
-                ctx.drawImage(mush, 750, Math.floor(Math.random()*275), 45, 40);
-                ctx.drawImage(sap, 750, Math.floor(Math.random()*275), 65, 50);
+        var theImage = new Image();
+
+        theImage.src = this.imgsrc;
+
+        theImage.onload= (() => {
+                ctx.drawImage(theImage, this.x, this.y, 45, 40);
             })
     }
         
     moveObstacle() {
         setInterval(() => {
-            this.x-=25;
-        },50)
+            this.x-=5;
+        }, 50);
     }
 }
     
@@ -136,15 +136,15 @@ let frames = 0;
     
 function animate() {
     setInterval(() => {
-        theGame.drawEverything();
-        if(frames % 2500 === 0) theGame.generateNewObstacle();
         ctx.clearRect(0,0,800,400);
-    theGame.obstacleCollisionCheck();
-    frames++;
-}, 1000)
+        theGame.drawEverything();
+        if(frames % 10 === 0) theGame.generateNewObstacle();
+        theGame.obstacleCollisionCheck();
+        frames++;
+}, 100)
 }
-
 //once this is working, use requestanimationframe instead of setinterval to stop the flickering
+
 
 function startGame() {
     theGame = new Game();
@@ -153,21 +153,16 @@ function startGame() {
 document.getElementById("btn-start").onclick = function() {
     startGame();
     animate();
-    theGame = new Game();
-    player = new Player();
-    player.drawPlayer();
-    obstacles = new Obstacle();
-    obstacles.drawObstacle();
 }
 
 document.onkeydown = function(e) {
     e.preventDefault();
     var move = e.keyCode;
     if (move === 37 || move === 39){
-        player.movePlayer(move);
+        theGame.player.movePlayer(move);
     }
     if (move === 32) {
-        player.jumpPlayer(move);
+        theGame.player.jumpPlayer(move);
     }
 }
 
